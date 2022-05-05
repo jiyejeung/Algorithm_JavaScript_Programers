279ee76ea20723200ce97b175aed2551e3acdec4
yejeungJi <jiyejeung@gmail.com>
2022-05-05 23:07:47 +0900

    홀수인 자연수를 골라 합을 구하는 함수, 1부터 N까지의 합을 출력하는 함수

--- index.js
+++ index.js
@@ -15,11 +15,33 @@ console.log(funcSolution02([3, 2, 5])); // false;
 /* 1인당 연필을 1자루씩 나누어 준다고 할때, N명의 학생수를 입력받고, 연필 몇 다스가 필요한지 출력하는 함수를 만들어라. */
 
 const funcSolution03 = (numArg) => {
+	return Math.ceil(numArg / 12);
+};
+
+console.log(funcSolution03(2));
+
+/* 자연수 N이 입력되면, 1부터 N까지의 합을 출력하는 함수를 만들어라. */
+
+const funcSolution04 = (numArg) => {
 	let answer = 0;
-	for(let i = 1; i <= numArg; i++) {
+	for (let i = 1; i <= numArg; i++) {
 		answer += i;
 	}
 	return answer;
-}
+};
 
-console.log(funcSolution03(5)); // 15
+console.log(funcSolution04(5)); // 15
+
+/* 자연수가 요소로 들어있는 배열이 입력되면, 그 중 가장 작은 최솟값을 출력하는 함수를 만들어라. */
+
+const funcSolution05 = (arrNumArg) => {
+	return Math.min(...arrNumArg);
+};
+
+console.log(funcSolution05([2, 5, 3, 4])); // 2
+
+const funcSolution06 = (arrNumArg) => {
+	return [arrNumArg.filter((value) => value % 2 == 1).reduce((pre, cur) => pre + cur, 0), Math.min(...arrNumArg.filter((value) => value % 2 == 1))];
+};
+
+console.log(funcSolution06([12, 44, 37, 63, 41, 53, 92, 85])); // [[37, 63, 41, 53, 85], 41]
